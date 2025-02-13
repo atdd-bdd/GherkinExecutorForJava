97badef9852e499cb5e93eb922cb02f36bbca1b5
atdd-bdd <37565969+atdd-bdd@users.noreply.github.com>
2025-02-12 23:41:49 -0500

    Still to fix more

---
+++ src/test/java/gherkinexecutor/Feature_Simple_Test/Feature_Simple_Test.java
@@ -0,0 +1,23 @@
+package gherkinexecutor.Feature_Simple_Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import java.util.List;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Feature_Simple_Test{
+
+    @Test
+    void test_Scenario_Simple(){
+         Feature_Simple_Test_glue Feature_Simple_Test_glue_object = new Feature_Simple_Test_glue();
+
+        List<ATest> objectList1 = List.of(
+             new ATest.Builder()
+                .anInt("1")
+                .aString("something")
+                .aDouble("1.2")
+                .build()
+                
+            );
+        Feature_Simple_Test_glue_object.Given_table_is(objectList1);
+        }
+    }
+
